f3e59f841827930c37e2a09130a7a6e903df8abe
unknown <Jungdeokho@gmail.com>
2014-12-05 22:56:15 +0900

    최대 약수의 개수 수정및 수의 범위 수정
    음수가 필요없어 unsigned int를 사용
    10자리수의 최대 약수의 개수는 2048개

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <malloc.h>
-int n=0;
+unsigned int n=0;
 void main(void) 
 {
   unsigned int p=0;
-  int getnum;
+  unsigned int getnum;
   int *divisor;
   puts("약수를 구하고자 하는 숫자를 입력해주세요.");
   printf(":");
   scanf("%d", &getnum);
-    divisor=(int *)malloc(sizeof(int)*(2+2(getnum/10)));
+    divisor=(int *)malloc(sizeof(int)*(2048)));
   get_divisor(n, getnum, getnum, 2);
   while(p<n) {
     printf("%d", divisor[p]);
@@ -17,9 +17,9 @@ void main(void)
   }
   printf("\n");
 }
-void get_divisor(extern int n, int scope, int num, int used) 
+void get_divisor(extern unsigned int n, unsigned int scope, unsigned int num, unsigned int used) 
 {
-  int i=used;
+  unsigned int i=used;
   while(i<=scope)
   {
     if(num%i==0) 
