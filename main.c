1453cc2c54a9831c568f2873ce6cf53abf7bb0ae
unknown <Jungdeokho@gmail.com>
2014-12-05 17:09:23 +0900

    source coding I

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+#include <malloc.h>
+void main(void) 
+{
+  int getnum;
+  int *primenum;
+  scanf("%d", &getnum);
+    primenum=(int *)malloc(sizeof(int)*(2+2(getnum/10)));
+  divider(getnum, getnum, 2);
+}
+void divider(int n, int scope, int num, int used) 
+{
+  int i=used;
+  while(i<scope)
+  {
+    if(num%i==0)
+      divider(num/i,num,i);
+      primenum[n]=i;
+      i++
+  }
+}
