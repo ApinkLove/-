ef7aea6880909f03a586605a615e0953d2edf58e
unknown <Jungdeokho@gmail.com>
2014-12-07 04:08:09 +0900

    Update main.c
    두밤을 세우니 만들어지네요

--- main.c
+++ main.c
@@ -1,39 +1,72 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <malloc.h>
-unsigned int n = 0; // 이 프로그램에서 두 함수의 연결고리
-int *divisor = NULL; // 포인터를 초기화
-int main(void) {
-	unsigned int p = 0;
+#include <stdlib.h>
+#include <conio.h>
+int *divisor = NULL;
+int *backpart = NULL;
+unsigned int cnt;
+void main(void) {
+	int sum_divisor(int);
+	/* 우애수 구하는 프로그램 */
+	unsigned int i = 1;
 	unsigned int getnum;
-	puts("약수를 구하고자 하는 숫자를 입력해주세요.");
+	divisor = (int *) malloc(sizeof(int) * 1024);
+	backpart = (int *) malloc(sizeof(int) * 1024);
+	puts("수를 입력하시오.");
 	printf(":");
-	scanf("%d", &getnum); // 사용자는 자신의 의도하는 수를 입력한다.
-	divisor = (int *) malloc(sizeof(int) * 2048); // int범위안 최대 약수의 개수는 2048개 이다.
-	get_divisor(n, getnum, getnum, 1);
-	while (p < n) {                  // 약수를 출력하게 했다. 이 부분부터는 사용자가 원하는대로 조작하면 된다.
-		printf("%d\n", divisor[p]);
-		p++;
-	}
-	free(divisor); // 메모리 누수 방지
-	printf("\n");
-	system("pause"); // 윈도우 종특이다 ;;
-	return 0;
-}
-void get_divisor(unsigned int cnt, unsigned int scope, unsigned int num,
-		unsigned int used) {
-	unsigned int i = used; // 전 과정에서 끊겻던 수 다음부터 이어가는 원리
-	while (i <= scope) {
-		if (num % i == 0) {
-			if (i == scope) {
-				divisor[cnt] = i; // 약수를 저장해둔다.
-			}
-			divisor[cnt] = i;
-			cnt++;
-			get_divisor(cnt, num / i, num, i + 1); // cnt는 소중하니까요 >.< , i+1 하는 이유는 다음에 그 다음수부터 이어 진행하라고
+	scanf("%d", &getnum);
+	while (i < 2147483648U) {
+		if (sum_divisor(getnum) == i && sum_divisor(i) == getnum) {
+			printf("우애수는 %d 입니다.\n", i);
+			break;
 		}
-		i++; // 반복이 끊길 수 있게 하기 위해
+		i++;
 	}
-	divisor[cnt] = num / i; // 곱해졌던 수도 결국은 약수
-	n = cnt; // 내가 임의로 출력하게 한 코드에서는 이 구문이 필요하다. 이게 없으면 반복이 불가능하다.
+	getch(); /* system(pause)을 대체합니다. */
+	free(divisor);
+}
+void calc_divisor(unsigned int n, unsigned int scope, unsigned int num,
+		unsigned int start) {
+	extern int unsigned cnt;
+	cnt = n;
+	unsigned int i = start;
+	while (i < scope) {
+		if (num % i == 0) {
+			if (i <= num / i) {
+				divisor[n] = i;
+				backpart[n] = num / i;
+				if (num / i > i + 1)
+					calc_divisor(n + 1, num / i, num, i + 1);
+				break;
+			}
+		}
+		i++;
+	}
+	/* merge */
+	if (i + 1 > num / i) {
+		n -= 1;
+		unsigned int p = n;
+		while (p != 0) {
+			if (divisor[n] != backpart[p]) {
+				n++;
+				divisor[n] = backpart[p];
+			}
+			cnt++;
+			p--;
+		}
+		free(backpart);
+	}
+}
+int sum_divisor(int num) {
+	unsigned int i = 0;
+	unsigned int sum_divisor = 0;
+	extern unsigned int cnt;
+	divisor[0] = 1;
+	calc_divisor(1, num, num, 2);
+	divisor[cnt] = num;
+	while (i < cnt) {
+		sum_divisor += divisor[i];
+		i++;
+	}
+	return sum_divisor;
 }
